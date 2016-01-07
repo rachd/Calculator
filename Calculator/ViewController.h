ff50153709207583d1d6765a01de85fb6ab1b298
Rachel Dorn <rachel.m.dorn@gmail.com>
2016-01-07 16:31:57 -0500

    Initial Commit

---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Rachel Dorn on 1/7/16.
+//  Copyright © 2016 Rachel Dorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Calculator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Calculator
+//
+//  Created by Rachel Dorn on 1/7/16.
+//  Copyright © 2016 Rachel Dorn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
